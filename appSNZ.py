57c8af5c857dcc8f4941bad423cd219206dd6283
davidloic10 <davidloic10@gmail.com>
2026-02-01 23:41:15 +0100

    correction

--- appSNZ.py
+++ appSNZ.py
@@ -6,7 +6,7 @@ import pickle
 import streamlit as st
 import os
 
-# Load CSS
+# lien avec le fichier css
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
